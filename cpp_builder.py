d688a5c621cfe25fc99bbc371dc5e4bafdcd58e0
viniputtim <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>
2025-05-20 19:24:04 -0300

    feat: add c++ build system

---
+++ cpp_builder.py
@@ -0,0 +1,137 @@
+import os
+import re
+import subprocess
+import sys
+import time
+
+
+DEBUG_MODE = True
+COMPILER = 'g++'
+CPP_VERSION = 'c++23'
+INCLUDE_DIR = 'include'
+SOURCE_DIR = 'source'
+OUTPUT_DIR = 'build'
+OUTPUT_EXT = '.out'
+
+
+def validate_args():
+    if len(sys.argv) < 2:
+        print('[ERROR]: Insufficient arguments.')
+        sys.exit(1)
+
+    if not sys.argv[1].endswith('.cpp'):
+        print('[ERROR]: Provided file is not a .cpp source file.')
+
+
+def get_library_flags(libraries):
+    library_flags = ''
+
+    if 'raylib' in libraries:
+        library_flags += '-lraylib -lGL -lm -lpthread -ldl -lrt -lX11'
+
+    library_flags = library_flags.split(' ') if library_flags else []
+
+    return library_flags
+
+
+def find_source_dir(file_path):
+    full_path = os.path.abspath(file_path)
+    parts = full_path.split(os.sep)
+
+    for i in range(len(parts), 0, -1):
+        if parts[i-1] == SOURCE_DIR:
+            return os.sep.join(parts[:i])
+
+    return None
+
+
+def find_source_files(source_dir):
+    source_files = []
+
+    if not source_dir:
+        print(f'[ERROR]: directory {SOURCE_DIR} not found.')
+        sys.exit(1)
+
+    for root, dirs, files in os.walk(source_dir):
+        for file in files:
+            if file.endswith('.cpp'):
+                source_files.append(os.path.join(root, file))
+
+    return source_files
+
+
+def validate_source_files(source_files):
+    if not source_files:
+        print('[ERROR]: source files not found.')
+        sys.exit(1)
+
+
+def to_snake_case(text):
+    snake_text = re.sub(r'([a-z])([A-Z])', r'\1_\2', text)
+    snake_text = re.sub(r'[-\s]', '_', snake_text)
+    snake_text = snake_text.lower()
+
+    return snake_text
+
+
+def build(build_command, project_dir):
+    result = subprocess.run(build_command, cwd=project_dir, capture_output=True, text=True)
+
+    return result
+
+
+def run(result, output, output_dir):
+    if result.returncode == 0:
+        subprocess.run([f'./{os.path.basename(output)}'], cwd=output_dir)
+    else:
+        print('[ERROR]: Compilation failed.')
+        print('Compiler error:')
+        print(result.stderr)
+
+
+def main():
+    start = time.time()
+    validate_args()
+
+    file_path = sys.argv[1]
+    libraries = sys.argv[2] if len(sys.argv) > 2 else ''
+
+    library_flags = get_library_flags(libraries)
+    source_dir = find_source_dir(file_path)
+    source_files = find_source_files(source_dir)
+
+    validate_source_files(source_files)
+
+    project_dir = os.path.abspath(os.path.join(source_dir, '..'))
+    include_dir = os.path.join(project_dir, INCLUDE_DIR)
+    output_dir = os.path.join(project_dir, OUTPUT_DIR)
+
+    project_name = os.path.basename(project_dir)
+    project_name = to_snake_case(project_name)
+    output = os.path.join(output_dir, f'{project_name}{OUTPUT_EXT}')
+
+    os.makedirs(output_dir, exist_ok=True)
+
+    build_command = [
+        COMPILER,
+        f'-std={CPP_VERSION}',
+        *(['-g'] if DEBUG_MODE else []),
+        '-o',
+        output,
+        *source_files,
+        '-I',
+        include_dir,
+        *library_flags
+    ]
+
+    result = build(build_command, project_dir)
+
+    end = time.time()
+    elapsed = end - start
+    print(f'[INFO]: Elapsed time: {elapsed:.2f} seconds.')
+
+    run(result, output, output_dir)
+
+
+if __name__ == '__main__':
+    main()
